3ee68d4717e853cde1a37b08891c2b2377172211
shukurudominic-design <shukurudominic@gmail.com>
2026-09-02 11:26:23 +0300

    Add Assignment 2 code

---
+++ assign_2.py
@@ -0,0 +1,8 @@
+# This program calculates the total cost of items
+
+price = float(input("100: "))
+quantity = int(input("5: "))
+
+total = 100 * 5
+
+print(f"{5} items at {5:.2f} each = {500:.2f}")
