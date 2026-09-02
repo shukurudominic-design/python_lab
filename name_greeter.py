4b3f6e0fcbf830233028894da5b3532c6aa4af4d
shukurudominic-design <shukurudominic@gmail.com>
2026-09-02 15:53:56 +0300

    Add name greeter program

--- name_greeter.py
+++ name_greeter.py
@@ -1,11 +1,11 @@
 # Ask the user to enter their full name
-full_name = input("SHUKURU DOMINIC: ")
+full_name = input("Enter your full name: ")
 
 # Split the name into separate parts
 name_parts = full_name.split()
 
 # Check whether the user entered two or more names
-if len("SHUKURU DOMINIC") >= 2:
-   print("Hello", "SHUKURU DOMINIC", "! Welcome!")
-else: print("SHUKURU.")
-print("SHUKURU DOMINIC")
+if len(name_parts) >= 2:
+    print(f"Hello, {name_parts[0]}! Welcome!")
+else:
+    print("Please enter your full name (first name and last name).")
