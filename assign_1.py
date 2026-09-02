645d5d998e33a640fbd593e5cca12675a6adfc6e
shukurudominic-design <shukurudominic@gmail.com>
2026-09-02 10:37:48 +0300

    Add Week 1 All About Me program

---
+++ assign_1.py
@@ -0,0 +1,11 @@
+# This program introduces me and my goal for the course
+
+name = "Shukuru"
+age = 22
+favourite_language = "Python"
+goal = "To improve my programming skills"
+
+print("My name is", "SHUKURU")
+print("I am", 22, "year old")
+print("My favourite programming language is", "PYTHON")
+print("My goal for this course is", "IMPROVE MY PROGRAMMING SKILLS")
